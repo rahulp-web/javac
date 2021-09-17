4697da100f19b60260517346e21a3ac5c3cd5cad
rahul <rahulprajapati855.rp@gmail.com>
2021-09-17 17:03:04 +0530

    typecasting

---
+++ typecasting.java
@@ -0,0 +1,23 @@
+package geekster;
+import java.util.Scanner;
+
+public class typecasting {
+	
+	public static void main (String[] args) {
+		
+		int b;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter any  words");
+		b = sc.nextInt();
+		
+		long d = b;
+		sc.close();
+		System.out.println(b);
+		System.out.println(d);
+		
+				
+		
+	}
+
+}
